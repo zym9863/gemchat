831556e8a8bbd8e4ec53383c2947d17f2e18bcb8
zym <202213093023@cuc.edu.cn>
2025-03-15 18:19:19 +0800

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "728514036"
+    serviceWorkerVersion: "1074791325"
   }
 });
